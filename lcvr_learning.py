d5417b8fb9f746c3f76fba6adcf5e4dbc6ecab1d
WildJimmy <jmcallister213@gmail.com>
2024-03-18 16:29:21 -0400

    get_training_data() now properly updates voltage channels and checks parameters each iteration

--- lcvr_learning.py
+++ lcvr_learning.py
@@ -112,7 +112,7 @@ class lcvr_learning:
         self.set_ch2_volts(1)
         self.outputs_on()
         
-        volt_range = np.linspace(0,20,realnum)
+        volt_range = np.linspace(0,20,realnum) #MAX VOLTAGE IS 20 V!
 
         # Now iterate across a wide range of voltage configs for channel 1 and 2 to record training data
         # Note response time of LCVR is ~30 ms max, so that limit is hard coded in right now. For speed
@@ -122,10 +122,12 @@ class lcvr_learning:
         
         #First keep ch2 constant and iterate over ch1
         for i in range(realnum):
-            time.sleep(.05)
+            self.check_params()
             ch1_volts = volt_range[i]
             ch2_volts = 1 #I know this is bad. Probably should just implement a function to read it straight will be quick
             self.set_ch1_volts(ch1_volts)
+            self.set_ch2_volts(ch2_volts)
+            time.sleep(.05)
             new_row = {'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage}
             trainingdata.append(new_row)
 
@@ -133,28 +135,34 @@ class lcvr_learning:
 
         #Now ch1 constant iterate over ch2
         for i in range(realnum):
-            time.sleep(.05)
+            self.check_params()
             ch1_volts = 1 #I know this is bad. Probably should just implement a function to read it straight will be quick
             ch2_volts = volt_range[i]
+            self.set_ch1_volts(ch1_volts)
             self.set_ch2_volts(ch2_volts)
+            time.sleep(.05)
             new_row = {'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage}
             trainingdata.append(new_row)
         
         #Now both increasing together
         for i in range(realnum):
-            time.sleep(.05)
+            self.check_params()
             ch1_volts = volt_range[i]
             ch2_volts = volt_range[i]
+            self.set_ch1_volts(ch1_volts)
             self.set_ch2_volts(ch2_volts)
+            time.sleep(.05)
             new_row = {'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage}
             trainingdata.append(new_row)
 
         #Now opposite directions
         for i in range(realnum):
-            time.sleep(.05)
+            self.check_params()
             ch1_volts = volt_range[i]
             ch2_volts = volt_range[len(volt_range) - i - 1]
+            self.set_ch1_volts(ch1_volts)
             self.set_ch2_volts(ch2_volts)
+            time.sleep(.05)
             new_row = {'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage}
             trainingdata.append(new_row)
 
