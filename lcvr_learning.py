647badc3c7cbdf837d9407a25f0c183a3c9f3436
WildJimmy <jmcallister213@gmail.com>
2024-03-25 07:47:13 -0400

    Updated data collection to record gain factor

--- lcvr_learning.py
+++ lcvr_learning.py
@@ -154,7 +154,7 @@ class lcvr_learning:
             self.set_input_volts(ch1_volts,1)
             self.set_input_volts(ch2_volts,2)
             time.sleep(delay)
-            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage()}
+            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Gain': gain, 'Out': self.get_voltage()}
             trainingdata.append(new_row)
 
         self.set_input_volts(min_volt,1)
@@ -168,7 +168,7 @@ class lcvr_learning:
             self.set_input_volts(ch1_volts,1)
             self.set_input_volts(ch2_volts,2)
             time.sleep(delay)
-            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage()}
+            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Gain': gain, 'Out': self.get_voltage()}
             trainingdata.append(new_row)
         
         #Now both increasing together
@@ -179,7 +179,7 @@ class lcvr_learning:
             self.set_input_volts(ch1_volts,1)
             self.set_input_volts(ch2_volts,2)
             time.sleep(delay)
-            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage()}
+            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Gain': gain, 'Out': self.get_voltage()}
             trainingdata.append(new_row)
 
         #Now opposite directions
@@ -190,7 +190,7 @@ class lcvr_learning:
             self.set_input_volts(ch1_volts,1)
             self.set_input_volts(ch2_volts,2)
             time.sleep(delay)
-            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Out': self.get_voltage()}
+            new_row = {'Wavelength': wavelength, 'V1': ch1_volts, 'V2': ch2_volts, 'Gain': gain, 'Out': self.get_voltage()}
             trainingdata.append(new_row)
 
         self.outputs_off()
